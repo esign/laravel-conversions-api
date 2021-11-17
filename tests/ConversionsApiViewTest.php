e333173a828fa81859995207dfd6d5407805bada
jordyvanderhaegen <jordy@esign.eu>
2021-11-17 21:10:31 +0100

    Update: move view tests into a single class

--- tests/ConversionsApiViewTest.php
+++ tests/ConversionsApiViewTest.php
@@ -3,16 +3,23 @@
 namespace Esign\ConversionsApi\Tests;
 
 use Esign\ConversionsApi\Facades\ConversionsApi;
-use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
+use Illuminate\Support\Facades\Blade;
 
 class ConversionsApiViewTest extends TestCase
 {
-    use InteractsWithViews;
+    /** @test */
+    public function it_can_render_the_conversions_api_page_view_directive()
+    {
+        ConversionsApi::shouldReceive('executePageViewEvent')->once();
+        Blade::compileString('@conversionsApiPageView');
+    }
 
     /** @test */
     public function it_can_render_the_data_layer_view()
     {
-        $view = $this->view('conversions-api::data-layer');
-        $view->assertSee(ConversionsApi::getEventId());
+        $this->assertStringContainsString(
+            ConversionsApi::getEventId(),
+            view('conversions-api::data-layer')->render()
+        );
     }
 }
