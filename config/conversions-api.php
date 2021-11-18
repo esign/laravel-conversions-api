1c96481bbbcf356ac024e024f8d26eaed18bd0a1
jordyvanderhaegen <jordy@esign.eu>
2021-11-18 14:17:53 +0100

    Update: comments for config variables

--- config/conversions-api.php
+++ config/conversions-api.php
@@ -1,7 +1,19 @@
 <?php
 
 return [
+    /**
+     * The access token used by the Conversions API.
+     */
     'access_token' => env('CONVERSIONS_API_ACCESS_TOKEN'),
+
+    /**
+     * The pixel ID used by the Conversions API.
+     */
     'pixel_id' => env('CONVERSIONS_API_PIXEL_ID'),
+
+    /**
+     * The Conversions API comes with a nice way to test your events.
+     * You may use this config variable to set your test code.
+     */
     'test_code' => null,
 ];
